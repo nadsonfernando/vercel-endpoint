c2e3ac1cbbde32527925a7fbc2f8dcebe3a8840b
Nadson Fernando <nadsonfernando@Nadsons-MacBook-Pro.local>
2024-11-28 11:57:01 -0300

    add domain associated

--- api/apple-app-site-association.ts
+++ api/apple-app-site-association.ts
@@ -4,7 +4,7 @@ export default (req: NowRequest, res: NowResponse) => {
   res.status(200).json({
     applinks: {},
     webcredentials: {
-      apps: ["XXXXXXXXXX.YYY.YYYYY.YYYYYYYYYYYYYY"],
+      apps: ["K24BXT3VBX.vercel-endpoint.vercel.app"],
     },
     appclips: {},
   });
