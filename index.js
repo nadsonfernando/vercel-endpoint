dfd8a3b78891615e4df4b0aaaf7b0016d662d7b0
Nadson Fernando <nadsonfernando@Nadsons-MacBook-Pro.local>
2024-11-28 10:59:35 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-webcredentials
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+  const response = {
+    applinks: {},
+    webcredentials: {
+      apps: ["XXXXXXXXXX.YYY.YYYYY.YYYYYYYYYYYYYY"]
+    },
+    appclips: {}
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em ${port}`);
+});
