71ae974889c0e5b27331a55ff5ea8f8c7d0e6bca
Nadson Fernando <nadsonfernando1@gmail.com>
2025-04-08 10:24:46 -0300

    change package name android

--- api/assetlinks.ts
+++ api/assetlinks.ts
@@ -6,7 +6,7 @@ export default (req: NowRequest, res: NowResponse) => {
       relation: ["delegate_permission/common.get_login_creds"],
       target: {
         namespace: "android_app",
-        package_name: "com.w3meetauth",
+        package_name: "w3meetauth.example",
         sha256_cert_fingerprints: [
           "00:6D:D0:76:8F:AB:A2:DC:47:AE:E5:28:2D:2A:DD:B9:DF:5A:B0:52:CC:7E:D5:2C:08:39:C2:4C:93:C6:60:13",
         ],
